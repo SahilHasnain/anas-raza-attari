1dec5e353fbdf0377a8566cc1a6bafd84a2e50bd
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-08-30 23:39:07 +0530

    Bump versionCode to 7

--- apps/mobile/brand.config.js
+++ apps/mobile/brand.config.js
@@ -19,7 +19,7 @@ module.exports = {
     packageId: "com.anasrazaattari", // production package
     packageIdDev: "com.anasrazaattari.dev",
     packageIdPreview: "com.anasrazaattari.preview",
-    versionCode: 6,
+    versionCode: 7,
     // Deep-link host for universal links (used in intent filters + associatedDomains)
     applinksHost: "anasrazaattari.expo.app",
   },
